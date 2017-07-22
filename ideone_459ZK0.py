7abf64d7d710ce0abf1cd69fc0f3f417390c910f
Darshonik <abhi1561995@gmail.com>
2017-07-22 14:09:05 +0530

    Add files via upload

---
+++ ideone_459ZK0.py
@@ -0,0 +1,17 @@
+# your code goes here
+for i in range(1,4):
+	for j in range(2,i-1,-1):
+		print(" "),
+	for k in range(i,0,-1):
+		print("*"),
+	for k in range(i,1,-1):
+		print("*"),
+	print
+for i in range(2,0,-1):
+	for j in range(2,i-1,-1):
+		print(" "),
+	for k in range(i,0,-1):
+		print("*"),
+	for k in range(i,1,-1):
+		print("*"),
+	print
