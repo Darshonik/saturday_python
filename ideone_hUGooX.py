e37bb8d15e2244b006bc3ebd2570c80d050a9380
Darshonik <abhi1561995@gmail.com>
2017-07-22 15:20:47 +0530

    Add files via upload

---
+++ ideone_hUGooX.py
@@ -0,0 +1,14 @@
+for i in range(1,4):
+	for j in range(2,i-1,-1):
+		print(" "),
+	for k in range(0,(2*i-1),+1):
+		print("*"),
+
+	print
+for i in range(2,0,-1):
+	for j in range(2,i-1,-1):
+		print(" "),
+	for k in range(0,(2*i-1),+1):
+		print("*"),
+	
+	print
